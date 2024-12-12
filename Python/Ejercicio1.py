39513d58b91775faff4bb711942e500c6cc1027f
Gabriela Segura <seguraramos99@gmail.com>
2024-12-11 21:50:20 -0600

    Agregar diccionario

---
+++ Python/Ejercicio1.py
@@ -0,0 +1,7 @@
+libros = {
+ "1984": {"autor": "George Orwell", "año": 1949, "género": "Distopía",
+"calificación": 4.8},"To Kill a Mockingbird": {"autor": "Harper Lee", "año": 1960, "género":
+"Ficción", "calificación": 4.9},
+ "El Gran Gatsby": {"autor": "F. Scott Fitzgerald", "año": 1925, "género":
+"Ficción", "calificación": 4.3}
+}
